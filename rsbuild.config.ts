c40b9108c2392b2a9de2be7754c9fd99cce06061
Rikka <admin@rikka.cc>
2026-04-27 09:27:10 +0800

    chore: add rsdoctor support and update dependencies in configuration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,16 @@
+import process from "node:process";
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
 import tailwind from "@tailwindcss/postcss";
 import { TanStackRouterRspack } from "@tanstack/router-plugin/rspack";
 
+const enableRsdoctor = Boolean(process.env.RSDOCTOR);
+
 export default defineConfig({
+  performance: {
+    ...(enableRsdoctor ? { buildCache: false } : {}),
+  },
   plugins: [pluginReact()],
   tools: {
     postcss: {
@@ -14,6 +21,18 @@ export default defineConfig({
     rspack: {
       plugins: [
         TanStackRouterRspack({ target: "react", autoCodeSplitting: true, routeFileIgnorePattern: ".css.d.ts", routeFileIgnorePrefix: "components" }),
+        ...(enableRsdoctor
+          ? [
+              new RsdoctorRspackPlugin({
+                output: {
+                  mode: "brief",
+                  options: {
+                    type: ["json"],
+                  },
+                },
+              }),
+            ]
+          : []),
       ],
     },
   },
